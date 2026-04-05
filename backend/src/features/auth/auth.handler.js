e05817f64123a69753f007beadcf1f6c3872e62c
Balaji C Hakari <balajihakari@gmail.com>
2026-04-05 18:38:15 +0530

    debug login error

--- backend/src/features/auth/auth.handler.js
+++ backend/src/features/auth/auth.handler.js
@@ -36,7 +36,11 @@ const login = async (req, res, next) => {
       token,
     });
   } catch (error) {
-    next(error);
+    console.log("LOGIN ERROR:", error);
+    return res.status(500).json({
+      message: error.message,
+    });
+    // next(error);
   }
 };
 
